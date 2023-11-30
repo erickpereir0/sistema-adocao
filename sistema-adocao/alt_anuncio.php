078406462757fc7552bc687b6fbf8c75e9213cd2
erickpereir0 <130837815+erickpereir0@users.noreply.github.com>
2023-11-30 11:39:09 -0300

    Atualizar o alt_anuncio.php

--- sistema-adocao/alt_anuncio.php
+++ sistema-adocao/alt_anuncio.php
@@ -4,49 +4,32 @@ require_once 'conectaBD.php';
 session_start();
 
 if (empty($_SESSION)) {
-  // Significa que as variáveis de SESSAO não foram definidas.
-  // Não poderia acessar aqui.
+  
   header("Location: index.php?msgErro=Você precisa se autenticar no sistema.");
   die();
 }
 
-/*
-//echo "Estou logado";
-echo '<pre>';
-print_r($_SESSION);
-print_r($_GET);
-echo '</pre>';
-die();
-*/
+
 
 $result = array();
 
-// Verificar se está chegando a informação (id_anuncio) pelo $_GET
 if (!empty($_GET['id_anuncio'])) {
 
-    // Buscar as informações do anúncio a ser alterado (no banco de dados)
   $sql = "SELECT * FROM anuncio WHERE email_usuario = :email AND id = :id";
   try {
     $stmt = $pdo->prepare($sql);
 
     $stmt->execute(array(':email' => $_SESSION['email'], ':id' => $_GET['id_anuncio']));
 
-    // Verificar se o usuário logado pode acessar/alterar as informações desse registro (id_anuncio)
     if ($stmt->rowCount() == 1) {
-      // Registro obtido no banco de dados
+      
       $result = $stmt->fetchAll();
       $result = $result[0]; // Informações do registro a ser alterado
 
-      /*
-      echo '<pre>';
-      print_r($result);
-      echo '</pre>';
-      */
-      //die();
-
+   
     }
     else {
-      //die("Não foi encontrado nenhum registro para id_anuncio = " . $_GET['id_anuncio'] . " e e-mail = " . $_SESSION['email']);
+      
       header("Location: index_logado.php?msgErro=Você não tem permissão para acessar esta página");
       die();
     }
@@ -58,12 +41,10 @@ if (!empty($_GET['id_anuncio'])) {
   }
 }
 else {
-  // Se entrar aqui, significa que $_GET['id_anuncio'] está vazio
   header("Location: index_logado.php?msgErro=Você não tem permissão para acessar esta página");
   die();
 }
 
-  // Redirecionar (permissão)
 
 ?>
 

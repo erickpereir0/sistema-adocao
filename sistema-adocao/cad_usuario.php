f861492ae832e32ebcbbf9992a8cc93ed7bd4cbb
erickpereir0 <130837815+erickpereir0@users.noreply.github.com>
2023-11-30 11:42:21 -0300

    Atualizar o cad_usuario.php

--- sistema-adocao/cad_usuario.php
+++ sistema-adocao/cad_usuario.php
@@ -5,13 +5,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   </head>
   <body>
-    <!--
-    Nome Completo
-    Data de Nascimento
-    Telefone
-    E-mail
-    Senha
-    -->
     <div class="container">
       <h1>Cadastrar Novo(a) Usuário(a)</h1>
       <form action="processa_usuario.php" method="post">

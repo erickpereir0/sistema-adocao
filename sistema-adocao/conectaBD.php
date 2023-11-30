302ea3bf3cecd1f467a0d6b0c9802039709830ab
erickpereir0 <130837815+erickpereir0@users.noreply.github.com>
2023-11-30 11:43:38 -0300

    Atualizar o conectaBD.php

--- sistema-adocao/conectaBD.php
+++ sistema-adocao/conectaBD.php
@@ -1,22 +1,12 @@
 <?php
-// endereco
-// nome do BD
-// usuario
-// senha
 
 $endereco = 'localhost';
 $banco = 'sispet';
 $usuario = 'postgres';
-$senha = 'Valerick1';
+$senha = '';
 
 try {
-  // sgbd:host;port;dbname
-  // usuario
-  // senha
-  // errmode
   $pdo = new PDO("pgsql:host=$endereco;port=5432;dbname=$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-  //echo "Conectado no banco de dados!!!";
 
 } catch (PDOException $e) {
   echo "Falha ao conectar ao banco de dados. <br/>";
